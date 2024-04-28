8780c113cb86a29bc6779cdcceb08d4f6400fb87
ENEmy <enemyr@pm.me>
2024-04-29 00:15:45 +0700

    Add Wav2Vec2ConformerTrainer class for training

---
+++ trainer/Wav2Vec2ConformerTrainer.py
@@ -0,0 +1,103 @@
+import torch
+from lightning.pytorch.utilities.types import STEP_OUTPUT, OptimizerLRScheduler
+from torch import optim, nn
+from torcheval.metrics import MulticlassAccuracy
+import lightning as L
+from models.Wav2Vec2ConformerCls import Wav2Vec2ConformerCls
+
+
+# TODO: maybe need to explicit clarify what inside config for the sake of save_hyperparameters()
+class Wav2Vec2ConformerTrainer(L.LightningModule):
+    def __init__(self, config: dict) -> None:
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = Wav2Vec2ConformerCls(**config)
+        self.criterion = nn.CrossEntropyLoss()
+        self.accuracy = MulticlassAccuracy()
+        self.config = config
+
+    def training_step(self, batch, batch_idx) -> STEP_OUTPUT:
+        X, y = batch
+        y_pred = self.model(X)
+        loss = self.criterion(y_pred, y)
+
+        y_pred_argmax = torch.argmax(y_pred, dim=1).to(torch.float32)
+        y_argmax = torch.argmax(y, dim=1).to(torch.float32)
+        self.accuracy.update(y_pred_argmax, y_argmax)
+        acc = self.accuracy.compute()
+        # acc = torch.sum(y_pred == y).item() / (len(y) * 1.0)
+
+        self.log_dict(
+            {"train_loss": loss, "train_accuracy": acc},
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+        return loss
+
+    def on_train_epoch_end(self) -> None:
+        self.accuracy.reset()
+
+    def validation_step(self, batch, batch_idx) -> STEP_OUTPUT:
+        X, y = batch
+        y_pred = self.model(X)
+        loss = self.criterion(y_pred, y)
+
+        y_pred_argmax = torch.argmax(y_pred, dim=1).to(torch.float32)
+        y_argmax = torch.argmax(y, dim=1).to(torch.float32)
+        self.accuracy.update(y_pred_argmax, y_argmax)
+        acc = self.accuracy.compute()
+        # acc = torch.sum(y_pred == y).item() / (len(y) * 1.0)
+
+        self.log_dict(
+            {"val_loss": loss, "val_accuracy": acc},
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+    def on_validation_end(self) -> None:
+        self.accuracy.reset()
+
+    def test_step(self, batch, batch_idx) -> STEP_OUTPUT:
+        X, y = batch
+        y_pred = self.model(X)
+        loss = self.criterion(y_pred, y)
+
+        y_pred_argmax = torch.argmax(y_pred, dim=1).to(torch.float32)
+        y_argmax = torch.argmax(y, dim=1).to(torch.float32)
+        self.accuracy.update(y_pred_argmax, y_argmax)
+        acc = self.accuracy.compute()
+        # acc = torch.sum(y_pred == y).item() / (len(y) * 1.0)
+
+        self.log_dict(
+            {"test_loss": loss, "test_accuracy": acc},
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+            logger=True,
+        )
+
+    def on_test_end(self) -> None:
+        self.accuracy.reset()
+
+    def configure_optimizers(self) -> OptimizerLRScheduler:
+        if self.config["optimizer"].upper() == "ADAM":
+            optimizer = optim.Adam(
+                self.parameters(),
+                lr=self.config["lr"],
+                betas=self.config["betas"],
+                eps=self.config["eps"],
+                weight_decay=self.config["weight_decay"],
+            )
+        else:
+            optimizer = optim.Adam(
+                self.parameters(),
+                lr=self.config["lr"],
+                betas=self.config["betas"],
+                eps=self.config["eps"],
+                weight_decay=self.config["weight_decay"],
+            )
+        return optimizer

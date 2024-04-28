735adbff7af56fc37ec852556bf3d8d59e4d546e
ENEmy <enemyr@pm.me>
2024-04-29 00:13:47 +0700

    Add Transpose module for reshaping tensors

---
+++ utils/Transpose.py
@@ -0,0 +1,10 @@
+from torch import nn, Tensor
+
+
+class Transpose(nn.Module):
+    def __init__(self, shape: tuple):
+        super(Transpose, self).__init__()
+        self.shape = shape
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.transpose(*self.shape)
